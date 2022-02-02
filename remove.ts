da6a45d1256de445fc77a76e92d86814c75466e3
alekseev <alekseev@selectel.ru>
2022-02-02 18:00:09 +0300

    miin fix

--- remove.ts
+++ remove.ts
@@ -1,28 +1,34 @@
 import { chromium } from "playwright";
 
+const email = "email";
+const password = "password";
+const nickname = "nick";
+
 (async () => {
   const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
   // Go to https://habr.com/ru/all/
-  await page.goto("https://habr.com/ru/users/Belisckner/favorites/posts/", { waitUntil: "domcontentloaded" });
+  await page.goto(`https://habr.com/ru/users/${nickname}/favorites/posts/`, { waitUntil: "domcontentloaded" });
   // Click [data-test-id="menu-toggle-guest"]
   await page.click('[data-test-id="menu-toggle-guest"]');
   // Click text=Войти
   await page.click("text=Войти");
   // Fill input[name="email"]
-  await page.fill('input[name="email"]', "<your_email>");
+  await page.fill('input[name="email"]', email);
   // Fill input[name="password"]
-  await page.fill('input[name="password"]', "<your_password>");
+  await page.fill('input[name="password"]', password);
   // Click text=Войти
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
   await page.click("text=Войти");
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(2000);
   // Click text=Убрать из закладок 315
-  while ((await page.textContent('[class="tm-tabs__tab-counter"]')) != "") {
+  await page.waitForURL(`https://habr.com/ru/users/${nickname}/favorites/posts/`)
+  while (await page.locator('article').nth(0).isVisible()) {
     while (await page.isVisible('[title="Убрать из закладок"]')) {
       await page.locator('[title="Убрать из закладок"]').nth(0).click();
       await page.waitForTimeout(50);
     }
     await page.reload();
   }
+  await browser.close();
 })();
